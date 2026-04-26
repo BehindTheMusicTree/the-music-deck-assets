bf4208d0e3c6e09e2f39152556f92cd215bf55a0
mignot <garcia.andreas.1991@gmail.com>
2026-04-26 10:14:08 +0200

    add lib/cards/index.ts

--- lib/cards/index.ts
+++ lib/cards/index.ts
@@ -1,5 +1,10 @@
-export { CARD_EXAMPLE_ART_BASE } from "./art-path";
+export { CARD_ARTWORK_BASE } from "./art-path";
 export { type CatalogEntry, CATALOG_ENTRIES } from "./catalog";
-export { MOCK_CARDS, WORLD_FLAG_CARDS, WORLD_MIXED_CARDS } from "./examples";
+export {
+  DECK_SPOTLIGHT_CARDS,
+  MOCK_CARDS,
+  WORLD_FLAG_CARDS,
+  WORLD_MIXED_CARDS,
+} from "./examples";
 export { DEFAULT_PREVIEW_CARD } from "./preview-default";
 export { type CardRarity, CARD_RARITY_ORDER } from "./card-rarity";
